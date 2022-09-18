a72fbb89f2c5bf75dbc43f12d2ea8d179284b624
vaishnavirao4 <113406601+vaishnavirao4@users.noreply.github.com>
2022-09-18 00:34:30 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Vaishnavi Santhapuri</Text>
+      <Text>301307031</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
